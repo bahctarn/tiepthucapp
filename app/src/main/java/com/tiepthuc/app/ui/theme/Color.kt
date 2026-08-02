6b3ed951a9256c22bd5835320520a11f160f064d
bahctarn <92133381+bahctarn@users.noreply.github.com>
2026-08-02 10:40:01 +0700

    Update Color.kt

--- app/src/main/java/com/tiepthuc/app/ui/theme/Color.kt
+++ app/src/main/java/com/tiepthuc/app/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.tiepthuc.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val OrangePrimary = Color(0xFFFF6B35)
-val OrangePrimaryDark = Color(0xFFE85A2A)
+val OrangePrimary = Color(0xFF063B00)
+val OrangePrimaryDark = Color(0xFF042A00)
 val GreenServed = Color(0xFF2E7D32)
 val AmberPending = Color(0xFFF57C00)
 val GreyEmpty = Color(0xFF9E9E9E)
